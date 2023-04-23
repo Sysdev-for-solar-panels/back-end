b4fd158957e0f9ba0829125a003868a3a9b95db0
green1490 <zambokrisztian1490@gmail.com>
2023-04-24 00:21:52 +0200

    change method from post to get

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -163,13 +163,12 @@ public class LoginController : ControllerBase
     }
 
     [Authorize(Roles = "raktarvezeto")]
-    [HttpPost("list-components")]
+    [HttpGet("list-components")]
     public async Task<ActionResult> ListComponent()
     {
       List<Component> component  = await new DBController().ListComponents();
       if (component.Count == 0)
       {
-        Console.WriteLine("Error");
         return BadRequest(JsonSerializer.Serialize(new {Message = "There is no component"}));
       }
       else
